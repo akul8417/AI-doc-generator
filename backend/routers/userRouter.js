a15d59b40fd63bed2b3ff6bec471ee7aa49d9a14
akul8417 <akulkushw66217>
2025-08-12 10:39:16 +0530

    backend

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,71 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Model = require('../models/usermodel');
+
+router.post('/add', (req,res) => {
+    console.log(req.body);
+
+    new Model(req.body).save()
+    .then((result) => {
+        res.status(200).json(result);
+        
+    }).catch((err) => {
+console.log(err);
+res.status(500).json(err);
+        
+    });
+    
+});
+router.get('/getall', (req,res) => {
+    Model.find()
+    .then((result) => {
+        res.status(200).json(result);
+        
+    }).catch((err) => {
+        res.status(500).json(err);
+        
+    });
+});
+router.get('/getbyemail/:email', (req,res) =>{
+    console.log(req.params.email);
+    model.findOne({email: req.params.email})
+    .then((result) => {
+        res.status(200).json(result);
+        
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+        
+        
+    });
+    
+});
+
+router.put('/update/:id', (rq,res)=>{
+    Model.findByIdAndUpdate(req.params.id, re.body, {new: true})
+    .then((result) => {
+        res.status(200).json(result);
+        
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+        
+        
+    });
+});
+router.get('/getbyid/:id', (req,res)=>{
+    Model.findById(req.params.id)
+    .then((result) => {
+        res.status(200).json(result);
+        
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+        
+        
+    });
+});
+
+module.exports = router;
