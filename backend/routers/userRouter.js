fbd7ce66f77e38e3355db1d6b372b4b0c52c32b6
akul8417 <akulkushw66217>
2025-08-12 10:45:52 +0530

    ..

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -4,68 +4,68 @@ const router = express.Router();
 
 const Model = require('../models/usermodels');
 
-router.post('/add', (req,res) => {
+router.post('/add', (req, res) => {
     console.log(req.body);
 
     new Model(req.body).save()
-    .then((result) => {
-        res.status(200).json(result);
-        
-    }).catch((err) => {
-console.log(err);
-res.status(500).json(err);
-        
-    });
-    
+        .then((result) => {
+            res.status(200).json(result);
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+
+        });
+
 });
-router.get('/getall', (req,res) => {
+router.get('/getall', (req, res) => {
     Model.find()
-    .then((result) => {
-        res.status(200).json(result);
-        
-    }).catch((err) => {
-        res.status(500).json(err);
-        
-    });
+        .then((result) => {
+            res.status(200).json(result);
+
+        }).catch((err) => {
+            res.status(500).json(err);
+
+        });
 });
-router.get('/getbyemail/:email', (req,res) =>{
+router.get('/getbyemail/:email', (req, res) => {
     console.log(req.params.email);
-    model.findOne({email: req.params.email})
-    .then((result) => {
-        res.status(200).json(result);
-        
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-        
-        
-    });
-    
+    model.findOne({ email: req.params.email })
+        .then((result) => {
+            res.status(200).json(result);
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+
+
+        });
+
 });
 
-router.put('/update/:id', (rq,res)=>{
-    Model.findByIdAndUpdate(req.params.id, re.body, {new: true})
-    .then((result) => {
-        res.status(200).json(result);
-        
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-        
-        
-    });
+router.put('/update/:id', (rq, res) => {
+    Model.findByIdAndUpdate(req.params.id, re.body, { new: true })
+        .then((result) => {
+            res.status(200).json(result);
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+
+
+        });
 });
-router.get('/getbyid/:id', (req,res)=>{
+router.get('/getbyid/:id', (req, res) => {
     Model.findById(req.params.id)
-    .then((result) => {
-        res.status(200).json(result);
-        
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-        
-        
-    });
+        .then((result) => {
+            res.status(200).json(result);
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+
+
+        });
 });
 
 module.exports = router;
